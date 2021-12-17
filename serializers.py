fb79a7e590c39ccbd8e02e5ca8eb44979065ac57
Philip Wentworth <guitartheist@gmail.com>
2021-12-17 17:54:50 -0500

    Added character_role field to Character serializer

--- serializers.py
+++ serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from whitebox.models import Character
 
 class CharacterSerializer(serializers.ModelSerializer):
+    character_role = serializers.CharField(source='get_role')
+
     class Meta:
         model = Character
-        fields = ['id', 'name', 'strength', 'dexterity', 'constitution', 'intelligence', 'wisdom', 'charisma', 'gold', 'silver', 'copper', 'user', 'finalized', 'c_role']
+        fields = ['id', 'name', 'strength', 'dexterity', 'constitution', 'intelligence', 'wisdom', 'charisma', 'gold', 'silver', 'copper', 'user', 'finalized', 'c_role', 'character_role']
